e38193ab5577ef83043788a0eb511256e08ed539
Juan Pablo Cervantes Gonzalez <g5ada02@hackademy.mx>
2020-03-28 22:47:51 -0600

    filter and display in models

--- gestionPedidos/models.py
+++ gestionPedidos/models.py
@@ -5,18 +5,17 @@ from django.db import models
 class Clientes(models.Model):
 
      nombre=models.CharField(max_length=30)
-     direccion=models.CharField(max_length=50)
-     email=models.EmailField()
+     direccion=models.CharField(max_length=50, verbose_name="La dirección")
+     email=models.EmailField(blank=True, null=True, verbose_name="El email")
      tfno=models.CharField(max_length=7)
-
+     def __str__(self):
+          return self.nombre
 class Articulos(models.Model):
      nombre=models.CharField(max_length=30)
      seccion=models.CharField(max_length=20)
      precio=models.IntegerField()
-
      def __str__(self):
-         return 'El nombre es %s la seccion es %s y el precio es %s'%(self.nombre, self.seccion, self.precio)
-
+          return self.nombre
 class Pedidos(models.Model):
      numero=models.IntegerField()
      fecha=models.DateField()
